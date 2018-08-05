54f5c5f41857fc3eba2b9c62cf7aea51fd38d437
VolleyTKE <chris.a.carr@gmail.com>
2018-08-05 00:16:20 -0500

    Base application

---
+++ OutlookAPIApp/src/main/java/com/cvg/oaa/config/SpringRootConfig.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cvg.oaa.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author ccarr
+ */
+
+@Configuration
+@ComponentScan(basePackages = {"com.cvg.capp.dao", "com.cvg.capp.service"})
+public class SpringRootConfig {
+    //To Do: Services, DAO, DataSource, Email Sender, ETC, Business Layer Beans
+    
+    
+    
+    //Ideally connections details are put in an xml file
+    @Bean
+    public BasicDataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/capp_db");
+        ds.setUsername("root");
+        ds.setPassword("test");
+        ds.setMaxTotal(2);
+        ds.setInitialSize(1);
+        ds.setTestOnBorrow(true);
+        ds.setValidationQuery("SELECT 1");
+        ds.setDefaultAutoCommit(true);
+        return ds;
+    }
+}
